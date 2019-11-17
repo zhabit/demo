052a1a2e09c6c360429af82cc177af3d3f887a0f
zhabit <1178449100@qq.com>
2019-11-17 20:08:18 +0800

    ch05 finished

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,16 @@
 
 <script>
 import Store from "./components/Store";
+import { mapActions } from "vuex";
 export default {
   name: "app",
-  components: { Store }
+  components: { Store },
+  methods: {
+    ...mapActions(["getData"])
+  },
+  created() {
+    this.getData();
+  }
 };
 </script>
 
--- src/data/index.js
+++ src/data/index.js
@@ -1,30 +1,34 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Axios from "axios";
 
 Vue.use(Vuex);
 
-const testData = [];
+const baseUrl = "http://localhost:3500";
+const productsUrl = `${baseUrl}/products`;
+const categoriesUrl = `${baseUrl}/categories`;
 
-for (let i = 1; i <= 10; i++) {
-  testData.push({
-    id: i, name: `Product #${i}`, category: `Category ${i % 3}`,
-    description: `This is Product #${i}`, price: i * 50
-  })
-}
 export default new Vuex.Store({
   strict: true,
   state: {
-    products: testData,
-    productsTotal: testData.length, currentPage: 1,
-    pageSize: 4
+    products: [],
+    categoriesData: [],
+    productsTotal: 0,
+    currentPage: 1,
+    pageSize: 4,
+    currentCategory: "All"
   },
+
   getters: {
-    processedProducts: state => {
+    productsFilteredByCategory: state => state.products.filter(p => state.currentCategory == "All" || p.category == state.currentCategory),
+    processedProducts: (state, getters) => {
       let index = (state.currentPage - 1) * state.pageSize;
-      return state.products.slice(index, index + state.pageSize);
+      return getters.productsFilteredByCategory.slice(index, index + state.pageSize);
     },
-    pageCount: state => Math.ceil(state.productsTotal / state.pageSize)
+    pageCount: (state, getters) => Math.ceil(getters.productsFilteredByCategory.length / state.pageSize),
+    categories: state => ["All", ...state.categoriesData]
   },
+
   mutations: {
     setCurrentPage(state, page) {
       state.currentPage = page;
@@ -32,6 +36,22 @@ export default new Vuex.Store({
     setPageSize(state, size) {
       state.pageSize = size;
       state.currentPage = 1;//改变分页大小时重置当前页为1
+    },
+    setCurrentCategory(state, category) {
+      state.currentCategory = category;
+      state.currentPage = 1;
+    },
+    setData(state, data) {
+      state.products = data.pdata;
+      state.productsTotal = data.pdata.length;
+      state.categoriesData = data.cdata.sort();
+    }
+  },
+  actions: {
+    async getData(context) {
+      let pdata = (await Axios.get(productsUrl)).data;
+      let cdata = (await Axios.get(categoriesUrl)).data;
+      context.commit("setData", { pdata, cdata });
     }
   }
 })
