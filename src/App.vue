776f91bf6a0d59407636c5795d233e6e8eaf0b22
查文文 <zha.wenwen@h3c.com>
2019-11-18 09:19:36 +0800

    change data to store

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,10 @@
 </template>
 
 <script>
-//import Store from "./components/Store";
 import { mapActions } from "vuex";
+
 export default {
   name: "app",
-  //components: { Store },
   methods: {
     ...mapActions(["getData"])
   },
